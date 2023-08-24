030a2b0b53c48cc371851fefc645a01cf8d0306b
ahmedtawfeeq1 <ahmed.tawfeeq@ejust.edu.eg>
2023-08-24 06:09:23 +0300

    update 001

--- server/generate.js
+++ server/generate.js
@@ -7,7 +7,7 @@ const generate = async (question) => {
     const davinciResponce = async (question) => {
         const response = await openai.completions.create({
             model: "text-davinci-003", // choose openai's model
-            prompt: `You are Ayman Taha, a fundraising expert consultant who helps startups raise fund, with a great track record of more than 30 million dollars raised.  Here is the user question to you as a coach: ${question}.`, 
+            prompt: `You are Mr. CEO, a fundraising expert consultant who helps startups raise fund, with a great track record of more than 30 million dollars raised.  Here is the user question to you as a coach: ${question}.`, 
             max_tokens: 1000,
             temperature: 1,
         }) // END OPENAI COMPLETIONS CREATE
@@ -16,7 +16,7 @@ const generate = async (question) => {
 
     const chatbotResponce = async (question) => {
         const messages = [
-            {role: "system", content: "Hello, you are Ayman Taha, a fundraising expert consultant who helps startups raise fund, with a great track record of more than 30 million dollars raised."},
+            {role: "system", content: "Hello, you are Mr. CEO, a fundraising expert consultant who helps startups raise fund, with a great track record of more than 30 million dollars raised."},
             {role: "user", content: `Answer this question about the sundraising & investment.  Here is the user question to you as a coach: \n\n What is CAC?.`},
             {role: "assistant", content: "it'scalled Customer Acquisition Cost. It's the cost of acquiring a new customer. It's calculated by dividing the total cost of acquiring new customers by the total number of new customers."},
             {role: "user", content: `Answer this question about the sundraising & investment.  Here is the user question to you as a coach: \n\n ${question}.`}
